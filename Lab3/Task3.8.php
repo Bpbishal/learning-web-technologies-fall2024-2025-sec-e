cd0ece95e1ea6e981b97db969aaaedfc4ea28eaf
Bishal Paul <123397462+Bpbishal@users.noreply.github.com>
2024-11-11 20:02:08 +0600

    Add files via upload

--- Lab3/Task3.8.php
+++ Lab3/Task3.8.php
@@ -1,10 +1,41 @@
 <?php
-    $array1=[
-                ['1','2','3'],
-                ['1','2'],
-                ['1']
-            ];
-    print("<html><table border='1'celspacing='0' align='center'><tr><td>");
-    
+
+// Declare 2D array
+$array = array(
+    array(1, 2, 3, 'A'),
+    array(1, 2, 'B', 'C'),
+    array(1, 'D', 'E', 'F')
+);
+
+// Print the array
+echo "Original Array:<br>";
+foreach ($array as $row) {
+    foreach ($row as $element) {
+        echo $element . " ";
+    }
+    echo "<br>";
+}
+
+
+// Pattern 1
+echo "Pattern 1:<br>";
+for ($i = 3; $i >= 1; $i--) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo $j . " ";
+    }
+    echo "<br>";
+}
+
+// Pattern 2
+echo "Pattern 2:<br>";
+$char = 'A';
+$num = 1;
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo $char++ . " ";
+    }
+    echo "<br>";
+}
+
 
 ?>
